c88aae5aacd172bdffe806ece2e372f83cea4b3f
Michael Holborn <holbyone@gmail.com>
2017-08-01 18:36:37 +1000

    My Message

--- djreact/settings.py
+++ djreact/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'djreact/static/root'))
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'djreact/static'),
 ]
